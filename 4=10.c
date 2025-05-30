62ee9cc5b219f9a860f16b55cc24ece6d4a9d4d5
chai-bukris <haybukris@gmail.com>
2025-05-30 17:23:12 +0300

    hope

--- 4=10.c
+++ 4=10.c
@@ -1,21 +1,21 @@
 #include <stdio.h> 
 #include <stdbool.h>
-
+#define numofnums 4
 /*
 If im new to this code and trying to understad whats going on, it will be very hard to me.
 one reason is that there are lots of numbers and i cant undderstand whats their MEANING.
-what is this 4? what is that 3?
+what is this 4? what is that 3? fixed 
 I addition, lets assume that for some reason, you were asked to be able to get 5 numbers and not only 4. what a shame...
 you will need to change every time that 4 apears to 5. that will take you a lot of time. (and we put aside the logical problems)
 I want you to change/add code, in a way that you will be able to change only 1 codeline in order to change the numbers count.
-HINT: read about MAGIC NUMBER. you are already familiar with them, and this is their main purpose.
+HINT: read about MAGIC NUMBER. you are already familiar with them, and this is their main purpose. fixed
 */ 
 
 double calculate(int nums[], char ops[]) {
-	double help[4];
+	double help[numofnums];
 	help[0] = nums[0];
 	int saman = 0;
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < numofnums - 1; i++) {
 		if (ops[i] == '*') {
 			help[saman] *= nums[i + 1];
 		}
@@ -42,14 +42,11 @@ double calculate(int nums[], char ops[]) {
 	return help[0];
 }
 int main() {
-	int numbers[4];
-	int arr[4];
+	int numbers[numofnums];
+	int arr[numofnums];
 	printf("Enter 4 numbers between 0 and 9: ");
-	arr[0] = scanf_s("%d", &numbers[0]);
-	arr[1] = scanf_s("%d", &numbers[1]);
-	arr[2] = scanf_s("%d", &numbers[2]);
-	arr[3] = scanf_s("%d", &numbers[3]);
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < numofnums; i++) {
+		arr[i] = scanf_s("%d", &numbers[i]);
 		if (arr[i] != 1) {
 			printf("bruh, I told you to enter numbers");
 			return 0;
@@ -60,27 +57,27 @@ int main() {
 		}
 	}
 	char operations[] = {'+', '-', '*', '/'};
-	char ops[3];
-	int nums[4];
-	for (int m = 0; m < 4; m++) {
+	char ops[numofnums - 1];
+	int nums[numofnums];
+	for (int m = 0; m < numofnums; m++) {
 		nums[0] = numbers[m];
-		for (int a = 0; a < 4; a++) {
+		for (int a = 0; a < numofnums; a++) {
 			ops[0] = operations[a];
-			for (int n = 0; n < 4; n++) {
+			for (int n = 0; n < numofnums; n++) {
 				if (m == n) {
 					continue;
 				}
 				nums[1] = numbers[n];
-				for (int b = 0; b < 4; b++) {
+				for (int b = 0; b < numofnums; b++) {
 					ops[1] = operations[b];
-					for (int o = 0; o < 4; o++) {
+					for (int o = 0; o < numofnums; o++) {
 						if (o == n || o == m) {
 							continue;
 						}
 						nums[2] = numbers[o];
-						for (int c = 0; c < 4; c++) {
+						for (int c = 0; c < numofnums; c++) {
 							ops[2] = operations[c];
-							for (int p = 0; p < 4; p++) {
+							for (int p = 0; p < numofnums; p++) {
 								if (p == n || p == m || p == o) {
 									continue;
 								}
