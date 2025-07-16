a8ffae0edd4776067826c864f8ff2ecf21b406f5
chai-bukris <haybukris@gmail.com>
2025-07-16 12:41:19 +0300

    now

--- 4=10_header.h
+++ 4=10_header.h
@@ -4,15 +4,11 @@ char operations[] = { '+', '-', '*', '/' };
 int numbers[NUM_OF_NUMS];
 char temp_ops[NUM_OF_NUMS - 1];
 bool used[NUM_OF_NUMS];
-double basic_calc(double num1, double num2, char op) { // basic calculation function
-	switch (op) {
-	case '+':
-		return num1 + num2;
-	case '-':
-		return num1 - num2;
-	case '/':
-		return num1 / num2;
-	case '*':
-		return num1 * num2;
-	}
-}
+double basic_calc(double num1, double num2, char op);
+int multiply_and_divide(int nums[], char ops[], double result[]);
+double add_and_substract(double result[], int index, char ops[]);
+double calculate_all_numbers(int nums[], char ops[]);
+void base_4(char ops[], int i);
+int combine_operators(int nums[]);
+int find_exercise(int numbers_filled, int current_numbers[]);
+int verify(int numbers[]);
